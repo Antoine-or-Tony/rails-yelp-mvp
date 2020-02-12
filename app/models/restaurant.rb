ea7bea53f209ef650abe033c981462a04684cc7d
Antoine Delannoy <antoine.delannoy@hotmail.de>
2020-02-12 12:32:45 +0100

    step 1

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+  belongs_to :name, :address, :phone_number, :category
+  has_many :reviews
+  validates :name, :address, :phone_number, :category, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_110610) do
+ActiveRecord::Schema.define(version: 2020_02_12_113157) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2020_02_12_110610) do
     t.integer "phone_number"
     t.string "category"
     t.string "review"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
